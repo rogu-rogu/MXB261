66874867b0dbfef8752ac5ff4a6d6ac5d5c8682b
rogu-rogu <167042789+rogu-rogu@users.noreply.github.com>
2024-08-21 15:44:50 +1000

    Adding the main file and the provided extra code

---
+++ Activity2_skeletoncode.m
@@ -0,0 +1,19 @@
+
+% set the variable y equal to 0
+y = 
+
+% set up a for loop to iterate 10 times through the below code
+for i = 1:10
+
+    % generate a random number u from the uniform distribution rand
+    u = 
+    
+    %set up an if... else statement the increases y by 1 if u<0.3
+    if u< 0.3
+        % increase y by 1
+
+    else    
+        % y stays the same
+
+    end
+end
